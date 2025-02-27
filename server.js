de8440b4e56b2df23ca265b28a634c3770477b66
AnkurRam2002 <ankurram2002@gmail.com>
2025-02-27 11:33:17 +0530

    added schema models

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI    
+).then(() => console.log('MongoDB connected')
+).catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
