ae981d6d4781d64d52a751950618d41c80cda6fe
AnkurRam2002 <ankurram2002@gmail.com>
2025-02-27 17:28:21 +0530

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const authRoutes = require('./routes/auth');
 const eventRoutes = require('./routes/events');
@@ -8,6 +9,7 @@ const eventRoutes = require('./routes/events');
 dotenv.config();
 
 const app = express();
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
