34a1a3fa56445e8643af1355ae1cc946b29d3044
AnkurRam2002 <ankurram2002@gmail.com>
2025-03-05 12:57:58 +0530

    minor adjustments

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 
-mongoose.connect(process.env.MONGO_URI    
-).then(() => console.log('MongoDB connected')
-).catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
